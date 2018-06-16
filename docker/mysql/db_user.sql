5bd8f18517f677b5d93d843a98952e7ed4116339
markterence <markterencetiglao@gmail.com>
2018-06-16 17:57:38 +0800

    removed crete user

--- docker/mysql/db_user.sql
+++ docker/mysql/db_user.sql
@@ -1,5 +1,4 @@
 -- create user for mysql
-CREATE USER 'todoserver'@'%' IDENTIFIED BY 'todoserverpassword';
 GRANT ALL PRIVILEGES ON *.* TO 'todoserver'@'%' IDENTIFIED BY 'todoserverpassword';
 -- reload mysql users
 FLUSH PRIVILEGES;
