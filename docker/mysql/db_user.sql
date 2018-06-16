4a603a7d56d8b1c584af7ef0212a90daf9236ca8
markterence <markterencetiglao@gmail.com>
2018-06-16 16:34:35 +0800

    updated db_user.sql

--- docker/mysql/db_user.sql
+++ docker/mysql/db_user.sql
@@ -1,6 +1,6 @@
 -- create user for mysql
 CREATE USER 'todoserver'@'%' IDENTIFIED BY 'todoserverpassword';
-GRANT ALL PRIVILEGES ON *.* TO 'todoserver'@'%' IDENTIFIED BY 'todoserverpassword' WITH GRANT OPTION;
-
+GRANT ALL PRIVILEGES ON *.* TO 'todoserver'@'%' IDENTIFIED BY 'todoserverpassword';
+--WITH GRANT OPTION;
 -- reload mysql users
 FLUSH PRIVILEGES;
