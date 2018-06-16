0f137dbef1603df5868b303591dcc380d4792373
markterence <markterencetiglao@gmail.com>
2018-06-16 12:16:00 +0800

    added pw

--- docker/mysql/db_user.sql
+++ docker/mysql/db_user.sql
@@ -1,6 +1,6 @@
 -- create user for mysql
 CREATE USER 'todoserver'@'%' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'todoserver'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'todoserver'@'%' IDENTIFIED BY 'todoserverpassword' WITH GRANT OPTION;
 
 -- reload mysql users
 FLUSH PRIVILEGES;
